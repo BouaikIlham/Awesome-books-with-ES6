5b909ba36c5c4b7a59d0472eeeab6d39fa0f8db9
ilham bouaik <ibouaik@gmail.com>
2022-05-03 19:22:27 +0100

    add luxon file

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { addBook } from './modules/addBook.js';
 import { showListBook } from './modules/showListBook.js';
 import { deleteBook } from './modules/deleteBook.js';
+import { DateTime } from './modules/luxon.js';
+
+const displayTime = document.getElementById('date');
+const datetime = DateTime.utc().toLocaleString(DateTime.DATETIME_FULL);
+displayTime.textContent = datetime;
 
 const addBtn = document.querySelector('.add-btn');
 const bookList = document.querySelector('.book-list');
